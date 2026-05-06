76a69da6b90d25d19088e65667ed56829c9af19d
Im-Sahilkumar <sahilrajsahil28@gmail.com>
2026-05-06 22:54:06 +0530

    loopstat

---
+++ Day 4/loopstat.py
@@ -0,0 +1,18 @@
+# #  pass
+
+# # for a in range(10):
+# #         pass
+
+# # break 
+
+# for a in range(1,101):
+#     if a%3==0 and a%5==0:
+#         print(a)
+#         break
+
+# continue
+
+for a in range(1,10):
+    if a==2 and a==6:
+        continue
+    print(a)
