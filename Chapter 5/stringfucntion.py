350cafcb80a886b0e068509322957458121dbea2
Im-Sahilkumar <sahilrajsahil28@gmail.com>
2026-05-28 14:53:27 +0530

    stringfunction

---
+++ Chapter 5/stringfucntion.py
@@ -0,0 +1,25 @@
+# s = "hello world"
+# n = s.upper()
+# print(n)
+# n = s.lower()
+# print(n)
+# n = s.strip()
+# print(n)
+# n = s.replace("o", "0")
+# print(n)
+# n = s.split(" ")
+# print(n)
+# n = s.find("o")
+# print(n)
+# n = len(s)
+# print(n)
+
+# way =1
+# name = "John Doe"
+# age = 30
+# print(f"welcome {name}, you are {age} years old.")
+
+# way =2
+name = "John Doe"
+age = 30
+print("welcome {}, you are {} years old.".format(name, age))
